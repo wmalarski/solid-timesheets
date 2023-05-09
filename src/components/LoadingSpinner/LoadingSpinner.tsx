05d4ced21bbd04f324355431530450fa42a6772d
Wojciech Malarski <w.malarski@gmail.com>
2023-05-09 20:08:52 +0200

    packages bumped

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-import { useI18n } from "@solid-primitives/i18n";
-import type { Component } from "solid-js";
-
-export const LoadingSpinner: Component = () => {
-  const [t] = useI18n();
-
-  return <span>{t("loading")}</span>;
-};
